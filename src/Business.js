f102fd74d3e46dc36899841fb4acea05e8e1d9f4
Boricaweb <thanapolsrangkaew@gmail.com>
2025-12-07 20:45:20 +0700

    9th commit move the Business list generate from Business .js to BusinessList.js

--- src/Business.js
+++ src/Business.js
@@ -1,7 +1,6 @@
 //import Place  from './SearchBar' below
 //..............................//
-import restaurant_img from './restaurant_img.jpg'
-import {useState, useEffect, StrictMode} from 'react'
+import {useState, useEffect} from 'react'
 
 const Business = () => {
   //Set Hook useState part
@@ -50,39 +49,8 @@ const Business = () => {
 
   error && <h1>Something go wrong: {error}</h1>;
 
-  return (
-    <StrictMode>
-      <div className='row m-3'>
-        {data.map((item) => {
-          return (
-            <div  key={item.properties.lon} className='col-md-4 col-sm-6 my-3 p-4'>
-              <div className='my-card card'>
-                <img className='card-img-top' src={restaurant_img} alt='restuarant profile'/>
-                <div className='card-body'>
-                  <div className='res-name d-flex justify-content-center'>
-                    <h3>{item.properties.name}</h3>
-                  </div> 
-                  <div className='res-info py-3 d-flex justify-content-around'>
-                    <div className='address'>
-                      {item.properties.housenumber ? <p>{item.properties.housenumber}</p> : <p>Not available</p>}
-                      {item.properties.city ? <p>{item.properties.city}</p> : <p>Not available</p>}
-                      {item.properties.state ? <p>{item.properties.state}</p> : <p>Not available</p>}
-                      {item.properties.postcode ? <p>{item.properties.postcode}</p> : <p>Not available</p>}
-                    </div>
-                    <div className='category d-flex flex-column align-items-center'>
-                      {item.properties.datasource.raw.cuisine ? <p>{item.properties.datasource.raw.cuisine}</p> : <p>Not available</p>}
-                      {item.properties.datasource.raw.phone ? <p>{item.properties.datasource.raw.phone}</p> : <p>Not available</p>}
-                    </div>               
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>   
-    </StrictMode>   
-  )
-
+  return data;
 };
+
 //Export function
 export default Business;
--- src/BusinessList.js
+++ src/BusinessList.js
@@ -1,12 +1,40 @@
 import Business from "./Business";
 import { StrictMode } from "react";
+import restaurant_img from './restaurant_img.jpg'
 
 const BusinessList = () => {
 
   return (
     <StrictMode>
       <section className='restaurant container-fluid'>
-        <Business />
+        <div className='row m-3'>
+          {Business().map((item) => {
+            return (
+              <div  key={item.properties.lon} className='col-md-4 col-sm-6 my-3 p-4'>
+                <div className='my-card card'>
+                  <img className='card-img-top' src={restaurant_img} alt='restuarant profile'/>
+                  <div className='card-body'>
+                    <div className='res-name d-flex justify-content-center'>
+                      <h3 className='text-center'>{item.properties.name}</h3>
+                    </div> 
+                    <div className='res-info py-3 d-flex justify-content-around'>
+                      <div className='address'>
+                        {item.properties.housenumber ? <p>{item.properties.housenumber}</p> : <p>Not available</p>}
+                        {item.properties.city ? <p>{item.properties.city}</p> : <p>Not available</p>}
+                        {item.properties.state ? <p>{item.properties.state}</p> : <p>Not available</p>}
+                        {item.properties.postcode ? <p>{item.properties.postcode}</p> : <p>Not available</p>}
+                      </div>
+                      <div className='category d-flex flex-column align-items-center'>
+                        {item.properties.datasource.raw.cuisine ? <p>{item.properties.datasource.raw.cuisine}</p> : <p>Not available</p>}
+                        {item.properties.datasource.raw.phone ? <p>{item.properties.datasource.raw.phone}</p> : <p>Not available</p>}
+                      </div>               
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>   
       </section>
     </StrictMode>
     );  
